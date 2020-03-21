ce8e58c9729a839a30bfea6de36c1f67309666f7
eisverticker <eisverticker@posteo.de>
2020-03-21 22:11:45 +0100

    remove card from layout

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -7,7 +7,8 @@ import { AccountService } from 'app/core/auth/account.service';
 
 @Component({
   selector: 'jhi-main',
-  templateUrl: './main.component.html'
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
   private renderer: Renderer2;
