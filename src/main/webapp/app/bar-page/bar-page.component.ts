c590bfa2613e3d2524e655933b2fd5ca68298ff5
eisverticker <eisverticker@posteo.de>
2020-03-22 16:55:00 +0100

    adjust bar page to new design

--- src/main/webapp/app/bar-page/bar-page.component.ts
+++ src/main/webapp/app/bar-page/bar-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { IGastronomy } from 'app/shared/model/gastronomy.model';
 import { GastronomyService } from 'app/entities/gastronomy/gastronomy.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, flatMap } from 'rxjs/operators';
+
+enum PageMode {
+  SPLASH,
+  DETAIL
+}
 
 @Component({
   selector: 'jhi-bar-page',
@@ -12,15 +17,23 @@ import { map, flatMap } from 'rxjs/operators';
 })
 export class BarPageComponent {
   $gastronomy: Observable<IGastronomy | null>;
+  PageMode = PageMode;
+  pageMode = PageMode.DETAIL;
 
-  constructor(protected gastronomyService: GastronomyService, private route: ActivatedRoute) {
+  constructor(protected gastronomyService: GastronomyService, private route: ActivatedRoute, private router: Router) {
     this.$gastronomy = this.route.params.pipe(
       flatMap(currentRoute => this.gastronomyService.find(currentRoute.operatorId)),
       map(httpEntity => httpEntity.body)
     );
   }
 
+  public enterDetailPage(): void {
+    this.pageMode = PageMode.DETAIL;
+  }
+
   public enterPub(): void {
-    alert('not implemented yet');
+    this.route.params.subscribe(route => {
+      this.router.navigate(['chat', route.operatorId]);
+    });
   }
 }
