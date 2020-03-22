ed1be94e91f3e548c0520ab19b8482cce0703e37
Michael Cornel <michael@stieler.it>
2020-03-22 13:53:31 +0100

    GoFundMe Embed instead of link

--- src/main/webapp/app/entities/gastronomy/gastronomy-detail.component.ts
+++ src/main/webapp/app/entities/gastronomy/gastronomy-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { JhiDataUtils } from 'ng-jhipster';
 
@@ -10,11 +11,18 @@ import { IGastronomy } from 'app/shared/model/gastronomy.model';
 })
 export class GastronomyDetailComponent implements OnInit {
   gastronomy: IGastronomy | null = null;
+  goFundMeEmbedUrl: SafeUrl | null = null;
 
-  constructor(protected dataUtils: JhiDataUtils, protected activatedRoute: ActivatedRoute) {}
+  constructor(protected dataUtils: JhiDataUtils, protected activatedRoute: ActivatedRoute, protected sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ gastronomy }) => (this.gastronomy = gastronomy));
+    this.activatedRoute.data.subscribe(({ gastronomy }) => {
+      this.gastronomy = gastronomy;
+      // TODO: Make sure no garbage is in gofundName
+      this.goFundMeEmbedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+        'https://www.gofundme.com/mvc.php?route=widgets/mediawidget&fund=' + gastronomy.gofundmeName + '&image=1&coinfo=1&preview=1'
+      );
+    });
   }
 
   byteSize(base64String: string): string {
